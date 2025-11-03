ca48922ca1c1b75d90488966a425c5b80a6fd761
namrata chandekar <namratachandekar9-netizen@gmail.com>
2025-11-03 07:24:58 -0800

    first commit

---
+++ a19.py
@@ -0,0 +1,20 @@
+#Simple math calculator
+a = int(input('Enter a number : '))
+op = input('Enter Operator (+, -, *, /) : ')
+b = int(input('Enter a number : '))
+
+if op == '+':
+    res = a + b
+    print('Result = {}'.format(res))
+elif op == '-':
+    res = a - b
+    print('Result = {}'.format(res))
+elif op == '*':
+    res = a * b
+    print('Result = {}'.format(res))
+elif op == '/':
+    res = a / b
+    print('Result = {}'.format(res))
+else:
+    print("Not a match operator")
+    
